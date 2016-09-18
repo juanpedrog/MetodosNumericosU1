c58a2cde0d62fcb6d7cc59d054bc581a5b1913c5
Juan Pedro Gil Llanos <juanpedrogilllanos@hotmail.com>
2016-09-18 00:29:45 -0600

    First Commit
    Creacion de la interfaz e implementacion de la funcion e^x

---
+++ MetodosNumericosU1/src/RowFormat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan Pedro Gil
+ */
+public class RowFormat {
+    protected int it;
+    protected double va;
+    protected String ea;
+    public RowFormat(int ite,double vap,String eap){
+        it=ite;
+        va=vap;
+        ea=eap;
+    }
+}
